2ab4c65fdb00ce1879dbaf03184a49bccad850d6
erdemez64@hotmail.com <erdemez64@hotmail.com>
2021-02-02 03:06:40 +0300

    2 Şubat Ders Sonu

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayı1 = 10;
+            int sayı2 = 30;
+            sayı1 = sayı2;
+            sayı2 = 65;
+            //sayi1 ??
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] 999
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendimi tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayısı = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+          
+           
+
+            
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+
+           
+
+
+        }
+    }
+}
