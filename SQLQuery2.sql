3e62a5c2652dfde3cf4ac4b0036b6251326b7c5e
erdemez64@hotmail.com <erdemez64@hotmail.com>
2021-02-20 16:08:37 +0300

    20.02.2021 en son 2

---
+++ SQLQuery2.sql
@@ -0,0 +1,42 @@
+﻿--Select
+Select ContactName Adi,CompanyName Şirketadi,City Şehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+select * from Products where categoryId=1 or categoryId=3
+
+select * from Products where categoryId=1 and UnitPrice>=10
+
+select * from Products where categoryId=1 order by UnitPrice desc   --ascending   --descending	
+
+Select count (*)Adet from Products 
+
+select categoryID,count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID,Products.ProductName,Products.UnitPrice,Categories.CategoryName 
+from Products inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where UnitPrice>10
+
+
+--	DTO Data Transformation Object
+
+Select * from Products p left	join [Order Details] od
+on p.ProductId = od.ProductID
+inner join Orders o
+on o.OrderID = od.OrderID
+	
+
+select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
+where o.CustomerID is null
+
+
+
+
+
+
+
+
+
